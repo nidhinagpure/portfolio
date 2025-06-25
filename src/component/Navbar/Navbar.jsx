d3c6919fd3a72af1235cd31426a9d4c39e924d0a
nidhinagpure <nagpurenidhi8@gmail.com>
2025-06-26 00:08:39 +0530

    navbar over

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,10 +1,22 @@
 import './../Navbar/Navbar.css';
 
+import Plogo from './../../assets/nidhi-logo.png';
+
 
 const Navbar = () => {
   return (
-    <div>
-        
+    <div className='navbar'>
+        <img src={ Plogo} atl="logo-portfolio" className='p-logo'/>
+        <ul className='nav-menu'>
+            <li>Home</li>
+            <li>About Me</li>
+            <li>Services</li>
+            <li>Skills</li>
+            <li>Contact</li>
+            <li>Project</li>
+        </ul>
+        <div className='nav-connect'>Connect With Me</div>
+
       
     </div>
   )
