26d4b01bd8987da3198d62cfb214091d074471dd
nidhinagpure <nagpurenidhi8@gmail.com>
2025-06-25 21:23:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from './component/Navbar/Navbar';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import './../Navbar/Navbar.css';
+
+
+const Navbar = () => {
+  return (
+    <div>
+        
+      
+    </div>
+  )
+}
+
+export default Navbar;
