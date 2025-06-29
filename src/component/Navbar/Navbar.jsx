fcf525e372f8519f8832516ace7456b4bd689d64
nidhinagpure <nagpurenidhi8@gmail.com>
2025-06-29 23:42:33 +0530

    baseurl paste

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -12,12 +12,12 @@ const Navbar = () => {
     <div className='navbar'>
         <img src={ Plogo} atl="logo-portfolio" className='p-logo'/>
         <ul className='nav-menu'>
-            <li><AnchorLink className='a-link' offset={50} href='#home'><p onClick={()=>setmenu("home")}>Home</p>{menu==="home"?<img src={Underline} alt="" style={{height:"10px", paddingBottom:"2px"}}/>:<></>}</AnchorLink></li>
-            <li><AnchorLink className='a-link'  offset={50}  href='#about' ><p onClick={()=>setmenu("about")}>About Me</p>{menu==="about"?<img src={Underline} alt="" style={{height:"10px", paddingBottom:"2px"}}/>:<></>}</AnchorLink></li>
-            <li><AnchorLink className='a-link'  offset={50} href='#skill'><p onClick={()=>setmenu("skill")}>Skills</p>{menu==="services"?<img src={Underline} alt="" style={{height:"10px", paddingBottom:"2px"}}/>:<></>}</AnchorLink></li>
-            <li><AnchorLink className='a-link'  offset={50} href='#services'><p onClick={()=>setmenu("services")}>Services</p>{menu==="skill"?<img src={Underline} alt="" style={{height:"10px", paddingBottom:"2px"}}/>:<></>}</AnchorLink></li>
-            <li><AnchorLink className='a-link'  offset={50} href='#project'><p onClick={()=>setmenu("project")}>Project</p>{menu==="project"?<img src={Underline} alt="" style={{height:"10px", paddingBottom:"2px"}}/>:<></>}</AnchorLink></li>
-            <li><AnchorLink className='a-link'  offset={50} href='#connects'><p onClick={()=>setmenu("connects")}>Contact</p>{menu==="connects"?<img src={Underline} alt="" style={{height:"10px", paddingBottom:"2px"}}/>:<></>}</AnchorLink></li>
+            <li><AnchorLink className='a-link' offset={50} href='#home'><p onClick={()=>setmenu("home")}>Home</p>{menu==="home"?<img src={Underline} alt="" style={{height:"10px"}}/>:<></>}</AnchorLink></li>
+            <li><AnchorLink className='a-link'  offset={50}  href='#about' ><p onClick={()=>setmenu("about")}>About Me</p>{menu==="about"?<img src={Underline} alt="" style={{height:"10px"}}/>:<></>}</AnchorLink></li>
+            <li><AnchorLink className='a-link'  offset={50} href='#skill'><p onClick={()=>setmenu("skill")}>Skills</p>{menu==="services"?<img src={Underline} alt="" style={{height:"10px"}}/>:<></>}</AnchorLink></li>
+            <li><AnchorLink className='a-link'  offset={50} href='#services'><p onClick={()=>setmenu("services")}>Services</p>{menu==="skill"?<img src={Underline} alt="" style={{height:"10px"}}/>:<></>}</AnchorLink></li>
+            <li><AnchorLink className='a-link'  offset={50} href='#project'><p onClick={()=>setmenu("project")}>Project</p>{menu==="project"?<img src={Underline} alt="" style={{height:"10px"}}/>:<></>}</AnchorLink></li>
+            <li><AnchorLink className='a-link'  offset={50} href='#connects'><p onClick={()=>setmenu("connects")}>Contact</p>{menu==="connects"?<img src={Underline} alt="" style={{height:"10px"}}/>:<></>}</AnchorLink></li>
 
         </ul>
         <AnchorLink className='a-link'  offset={50} href='#connects'><div className='nav-connect'>Connect Me</div></AnchorLink> 
